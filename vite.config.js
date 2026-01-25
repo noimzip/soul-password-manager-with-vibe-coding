22d02b93d1e5971001e90fa2a5de2a6868e31b4b
noimzip <gudoumaki@proton.me>
2026-01-25 10:35:32 +0900

    Change: Build an introduction site

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
+import { dirname, resolve } from 'path'
+import { defineConfig } from 'vite'
+const root = resolve(__dirname, './');
+const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
   // ビルド時の出力先設定など
+publicDir: 'public',
+  root,
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
+    outDir,
+    rollupOptions: {
+      input: {
+        main: resolve(root, './', './index.html'),
+        web: resolve(root, './web', './index.html')
+      },
+    },
   },
   base: "./",
   // ローカルサーバーの設定
